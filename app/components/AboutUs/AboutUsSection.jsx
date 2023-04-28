a986345eb61be3db2938d980c4819db8c063b5ff
hasan-rifat <developerhr000@gmail.com>
2023-04-28 10:06:33 +0600

    about hero section

---
+++ app/components/AboutUs/AboutUsSection.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const AboutUsSection = () => {
+  return (
+    <section>
+      <div className="container mx-auto px-20">
+        <div>
+          <div>
+            <h2></h2>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutUsSection;
