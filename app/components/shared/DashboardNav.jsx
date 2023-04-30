7c77f275544b67b088605aae388088bc82769932
Joy Paul <joypaul3592@gmail.com>
2023-05-01 00:48:32 +0600

    clear navbar

--- app/components/shared/DashboardNav.jsx
+++ app/components/shared/DashboardNav.jsx
@@ -4,19 +4,16 @@ import { RiAppsFill } from "react-icons/ri";
 import { BsChatDotsFill } from "react-icons/bs";
 import { BiBookAlt } from "react-icons/bi";
 import { RiContactsFill } from "react-icons/ri";
-import { SiGnuprivacyguard } from "react-icons/si";
 import {
   MdKeyboardArrowDown,
   MdOutlineKeyboardArrowRight,
 } from "react-icons/md";
-import {
-  AiOutlineCalendar,
-  AiFillFileAdd,
-  AiOutlineShoppingCart,
-  AiOutlineMail,
-  AiFillProject,
-} from "react-icons/ai";
+
 import Link from "next/link";
+import Image from "next/image";
+
+// Image Import
+import logo from "../../../images/logo/logo.png";
 const DashboardNav = () => {
   const [DropdownEnter, setDropdownEnter] = useState(-1);
 
@@ -69,9 +66,8 @@ const DashboardNav = () => {
     <div className=" h-full ">
       {/* <!-- Component Start --> */}
       <div
-        class={`flex flex-col  items-center justify-between h-full overflow-x-hidden scrollbar overflow-y-auto transition-all duration-300  ease-in-out bg-[#a9f8fd] text-[#405859b3] shadow-[0_0_15px_0_rgba(0,0,0,0.11)]  ${
-          close ? "w-16 duration-500" : "w-80 "
-        }`}
+        class={`flex flex-col  items-center justify-between h-full overflow-x-hidden scrollbar overflow-y-auto transition-all duration-300  ease-in-out bg-[#a9f8fd] text-[#405859b3] shadow-[0_0_15px_0_rgba(0,0,0,0.11)]  ${close ? "w-16 duration-500" : "w-80 "
+          }`}
       >
         <div className=" w-full flex flex-col justify-between ">
           <Link
@@ -79,38 +75,35 @@ const DashboardNav = () => {
             className={`w-full  ${close ? "px-0" : "px-4"}`}
           >
             <div
-              class={` w-full items-center mt-3 h-12  ${
-                close ? "  flex  items-center w-full ml-3" : "flex flex-row "
-              }`}
+              class={` w-full flex flex-col justify-center items-center mt-16  h-24 `}
             >
-              <div className={`${close ? "" : "py-1"}`}>
-                <svg
-                  onClick={() => {
-                    setClose(!close);
-                    setDropdownEnter(-1);
-                  }}
-                  class={`w-8 h-8  fill-current text-red-500 ${
-                    close && "mx-auto ml-1"
-                  } `}
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z" />
-                </svg>
+              {/* Bar icons */}
+              <div className={` absolute  top-4 right-[1.2rem] group `}>
+                <div onClick={() => {
+                  setClose(!close);
+                  setDropdownEnter(-1);
+                }}>
+                  <div className={`h-[2px] w-[21px] bg-black mb-1 `}></div>
+                  <div className={`h-[2px]  bg-black mb-1 transition-all duration-500 ease-in-out ${close ? 'w-[21px]' : 'w-[15px] group-hover:w-[21px]'}`}></div>
+                  <div className={`h-[2px]  bg-black transition-all duration-500 ease-in-out ${close ? ' w-[21px]' : 'w-2 group-hover:w-[21px]'}`}></div>
+                </div>
               </div>
 
-              <div className=" overflow-hidden">
+              <div >
+                <Image src={logo} alt="Picture of the author" className={` h-16 `} />
+              </div>
+
+              <div className=" overflow-hidden  ">
                 <span
-                  className={`transition-all duration-300 ease-out overflow-hidden text-xl leading-6 text-[#212936] font-medium ${
-                    close
-                      ? " translate-x-[-24rem] -ml-20  "
-                      : " translate-x-0 pl-2"
-                  }`}
+                  className={`transition-all duration-300 ease-out  overflow-hidden  text-xl leading-6 text-[#212936] font-medium ${close
+                    ? " translate-x-[-28rem] -ml-32 "
+                    : " translate-x-0 duration-500 -ml-0"
+                    }`}
                 >
-                  MyIEP <span className="text-[#367CFF]">Buddy</span>
+                  MyIEP <span>Buddy</span>
                 </span>
               </div>
+
             </div>
           </Link>
 
@@ -123,22 +116,17 @@ const DashboardNav = () => {
                   <Link
                     href={menu?.Path}
                     className={({ isActive }) =>
-                      `w-full mt-3 hover:bg-[#5a4e4c]  ${
-                        isActive ? "bg-[#5a4e4c]  rounded-b-none" : null
+                      `w-full mt-3 hover:bg-[#5a4e4c]  ${isActive ? "bg-[#5a4e4c]  rounded-b-none" : null
                       }`
                     }
                   >
                     <div
-                      class={` flex w-full items-center h-12 px-3  rounded transition-all duration-150 ease-in hover:pl-5   
-                                         
-                                            
-                                            `}
+                      class={` flex w-full items-center h-12 px-3  rounded transition-all duration-150 ease-in hover:pl-5`}
                     >
                       <div className={` ${close ? " py-1 " : "py-1"}`}>
                         <div
-                          class={` text-xl p-1  bg-red-500 rounded text-white  stroke-current  ${
-                            close && "mx-auto "
-                          } `}
+                          class={` text-xl p-1   rounded text-[#405859]  stroke-current  ${close && "mx-auto "
+                            } `}
                         >
                           {menu?.icon}
                         </div>
@@ -150,18 +138,16 @@ const DashboardNav = () => {
                         className=" overflow-hidden flex h-6 w-[80%]  "
                       >
                         <span
-                          className={`transition-all  w-[80%]  duration-500 ease-out overflow-hidden text-sm font-semibold ${
-                            close
-                              ? " translate-x-[-24rem] -ml-28  "
-                              : "duration-500 translate-x-0 pl-2"
-                          }`}
+                          className={`transition-all  w-[80%]  duration-500 ease-out overflow-hidden text-sm font-semibold ${close
+                            ? " translate-x-[-24rem] -ml-28  "
+                            : "duration-500 translate-x-0 pl-2"
+                            }`}
                         >
                           {menu?.Route}
                         </span>
                         <div
-                          className={`inline-flex h-full mt-1 float-right ${
-                            menu?.Service ? "block" : "hidden"
-                          }  `}
+                          className={`inline-flex h-full mt-1 float-right ${menu?.Service ? "block" : "hidden"
+                            }  `}
                         >
                           {DropdownEnter == index ? (
                             <MdKeyboardArrowDown className=" text-lg" />
