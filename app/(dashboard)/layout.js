683adef05a978a4a26d960f82d3cfdc14945eab0
hasan-rifat <developerhr000@gmail.com>
2023-05-02 14:27:26 +0600

    summery-every-genertor-s-results done

--- app/(dashboard)/layout.js
+++ app/(dashboard)/layout.js
@@ -4,11 +4,13 @@ import DashboardNav from "../components/shared/DashboardNav";
 import DashboardTopNav from "../components/shared/DashboardTopNav";
 
 export default function Layout({ children }) {
-
   // Side Navbar
-  const [close, setClose] = useState(JSON.parse(localStorage.getItem("close")));
+  const [close, setClose] = useState(
+    typeof window !== "undefined" && JSON.parse(localStorage.getItem("close"))
+  );
   useEffect(() => {
-    localStorage.setItem("close", JSON.stringify(close));
+    typeof window !== "undefined" &&
+      localStorage.setItem("close", JSON.stringify(close));
     setClose(close);
   }, [close]);
 
@@ -17,14 +19,16 @@ export default function Layout({ children }) {
       <div className="  fixed bottom-0 top-0 left-[5%7] z-[9999] ">
         <DashboardNav close={close} setClose={setClose} />
       </div>
-      <div className={` transition-all duration-500 ease-in-out ${close ? 'ml-16' : 'ml-80'}`}>
+      <div
+        className={` transition-all duration-500 ease-in-out ${
+          close ? "ml-16" : "ml-80"
+        }`}
+      >
         <div>
           <DashboardTopNav />
         </div>
 
-        <div >
-          {children}
-        </div>
+        <div>{children}</div>
       </div>
     </section>
   );
