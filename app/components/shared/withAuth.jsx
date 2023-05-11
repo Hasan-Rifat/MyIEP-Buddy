c9e506544cba2a7d7e404b9faca81c4bce5a2b0a
hasan-rifat <developerhr000@gmail.com>
2023-05-11 21:14:46 +0600

    work done

--- app/components/shared/withAuth.jsx
+++ app/components/shared/withAuth.jsx
@@ -2,15 +2,6 @@ import { useRouter } from "next/navigation";
 
 export function withAuth(WrappedComponent) {
   return function WithAuth(props) {
-    const token = JSON.parse(localStorage.getItem("user")).token;
-
-    const router = useRouter();
-    console.log(token);
-
-    /*   if (!token) {
-      router.push("/sign-in");
-    } */
-
     return <WrappedComponent {...props} />;
   };
 }
