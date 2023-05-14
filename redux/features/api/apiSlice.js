f05a5d9d8e9991fb06ca7156a9899b53ed8e0776
hasan-rifat <developerhr000@gmail.com>
2023-05-15 01:20:07 +0600

    done

--- redux/features/api/apiSlice.js
+++ redux/features/api/apiSlice.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://my-iep-buddy-server.vercel.app",
+    baseUrl: "https://my-iep-buddy-server.vercel.app/api/v1/",
   }),
 
   endpoints: (builder) => ({}),
